d21c497a43c6f7304384f62070914ae94502a7e5
MD. Saiful Islam <saifulcse47@gmail.com>
2024-07-13 01:51:26 +0100

    node file setup

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-import { createServer } from 'node:http';
+// const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+var http = require('http');
+var server = http.createServer(function (req, res) {
+    res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(8080);
+console.log('server running sucessfully');
